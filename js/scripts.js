f4ec08071cc2e50c8a8eca3328e5a6145aeef85c
Alex Noble and Noah Medoff <noah@>
2018-04-02 09:18:36 -0700

    create Destination object constructor

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business Logic
+
+function Destination(location, landmarks, date, notes){
+  this.location = location;
+  this.landmarks = landmarks;
+  this.date = date;
+  this.notes = notes;
+}
