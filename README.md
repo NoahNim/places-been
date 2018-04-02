495c2fa60019768c84cac3148448abf0aaf0133d
Alex Noble and Noah Medoff <alex@>
2018-04-02 09:47:48 -0700

    create user interface to print out destination

---
+++ README.md
@@ -0,0 +1,31 @@
+# places-been
+
+#### _Places You've Been_
+
+#### By _**Alex Noble and Noah Medoff **_
+
+## Description
+
+#### _This is a website for keeping track of all of the places that someone has been_
+
+## Setup
+
+#### _Open in a web browser._
+
+## Known Bugs
+
+#### _No known bugs._
+
+## Technologies Used
+
+#### _HTML_
+#### _CSS_
+#### _Bootstrap_
+#### _JQuery_
+#### _Javascript_
+
+## Legal
+
+#### _Copyright (c) 2018 Alex Noble, Noah Noah Medoff_
+
+#### _This software is licensed under the MIT license._
--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,24 @@ function Destination(location, landmarks, date, notes){
   this.date = date;
   this.notes = notes;
 }
+
+Destination.prototype.fullInfo = function () {
+  return this.location + " " + this.landmarks + " " + this.date + " " + this.notes;
+};
+
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedLocation = $("input#newLocation").val();
+    var inputtedLandmarks = $("input#newLandmarks").val();
+    var inputtedDate = $("input#newDate").val();
+    var inputtedNotes = $("input#newNotes").val();
+
+    var newDestination=new Destination(inputtedLocation, inputtedLandmarks, inputtedDate, inputtedNotes);
+
+    $("ul#placeList").append("<li><span class='place'>" + newDestination.fullInfo() + "</span></li>");
+  });
+
+
+});
